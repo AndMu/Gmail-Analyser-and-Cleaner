3c9cfcee0584220c68cac8b0f1ac05b084bcd4a4
Andrius Mudinas <keistokas@gmail.com>
2017-10-31 10:36:44 +0000

    Update

--- src/Wikiled.Gmail/Commands/CalculateCommand.cs
+++ src/Wikiled.Gmail/Commands/CalculateCommand.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Linq;
 using System.Net.Http;
 using Google.Apis.Gmail.v1.Data;
@@ -9,12 +10,21 @@ namespace Wikiled.Gmail.Commands
 {
     public class CalculateCommand : BaseListGmailCommand
     {
+        private ConcurrentBag<SenderHolder> senderHolders = new ConcurrentBag<SenderHolder>();
+
+        public override void Execute()
+        {
+            base.Execute();
+
+        }
+
         protected override void OnMessageCallback(Message content, RequestError error, int i, HttpResponseMessage message)
         {
             var from = content.Payload?.Headers.Where(item => string.Compare(item.Name, "From", StringComparison.OrdinalIgnoreCase) == 0).Select(item => item.Value).FirstOrDefault();
             if (!string.IsNullOrWhiteSpace(@from))
             {
                 SenderHolder holder = new SenderHolder(@from, content.SizeEstimate);
+                senderHolders.Add(holder);
             }
         }
     }
--- src/Wikiled.Gmail/Program.cs
+++ src/Wikiled.Gmail/Program.cs
@@ -1,70 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
-using Google.Apis.Auth.OAuth2;
-using Google.Apis.Gmail.v1;
-using Google.Apis.Gmail.v1.Data;
-using Google.Apis.Requests;
-using Google.Apis.Services;
-using Wikiled.Gmail.Analysis;
+using System.Reflection;
+using NLog;
+using Wikiled.Core.Utility.Arguments;
+using Wikiled.Gmail.Commands;
 
 namespace Wikiled.Gmail
 {
     public class Program
     {
+        private static readonly Logger log = LogManager.GetCurrentClassLogger();
+
         public static void Main(string[] args)
         {
-            
-            ProcessMessages(credential).Wait();
-            Console.Read();
-        }
+            log.Info("Starting {0} version utility...", Assembly.GetExecutingAssembly().GetName().Version);
 
-        private static async Task ProcessMessages(UserCredential credential)
-        {
-            // Create Gmail API service.
-            var service = new GmailService(
-                new BaseClientService.Initializer
-                {
-                    HttpClientInitializer = credential,
-                    ApplicationName = ApplicationName
-                });
+            List<Command> commandsList = new List<Command>();
+            commandsList.Add(new CalculateCommand());
+            commandsList.Add(new NewslettersCommand());
+            var commands = commandsList.ToDictionary(item => item.Name, item => item, StringComparer.OrdinalIgnoreCase);
 
-            var emailListRequest = service.Users.Messages.List("me");
-            emailListRequest.Q = "-label:chats";
-            emailListRequest.IncludeSpamTrash = false;
-            emailListRequest.MaxResults = 100;
-
-            do
+            try
             {
-                //get our emails
-                var emailListResponse = await emailListRequest.ExecuteAsync().ConfigureAwait(false);
-                emailListRequest.PageToken = emailListResponse.NextPageToken;
-                if (emailListResponse?.Messages != null)
+                if (args.Length == 0)
                 {
-                    // Create a batch request.
-                    var request = new BatchRequest(service);
-                    //loop through each email and get what fields you want...
-                    foreach (var email in emailListResponse.Messages)
-                    {
-                        request.Queue<Message>(service.Users.Messages.Get("me", email.Id),
-                            (content, error, i, message) =>
-                            {
-                                var from = content.Payload?.Headers.Where(item => string.Compare(item.Name, "From", StringComparison.OrdinalIgnoreCase) == 0).Select(item => item.Value).FirstOrDefault();
-                                var unsubscribe = content.Payload?.Headers.Where(item => string.Compare(item.Name, "List-Unsubscribe", StringComparison.OrdinalIgnoreCase) == 0).Select(item => item.Value).FirstOrDefault();
-                                if (!string.IsNullOrWhiteSpace(from))
-                                {
-                                    SenderHolder holder = new SenderHolder(from, content.SizeEstimate);
-                                }
-                            });
-                    }
-
-                    await request.ExecuteAsync().ConfigureAwait(false);
-                    await Task.Delay(TimeSpan.FromSeconds(1.5)).ConfigureAwait(false);
+                    log.Warn("Please specify arguments");
+                    CommandLineParser.PrintCommands(commands.Values);
+                    return;
                 }
-            } while (!String.IsNullOrEmpty(emailListRequest.PageToken));
+
+                if (!commands.TryGetValue(args[0], out var command))
+                {
+                    log.Error("Unknown Command");
+                    return;
+                }
+
+                command.ParseArguments(args.Skip(1));
+                command.Execute();
+            }
+            catch (Exception ex)
+            {
+                log.Error(ex);
+            }
         }
     }
 }
